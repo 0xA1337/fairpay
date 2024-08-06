c3e59041f35161b672ba1289c3d6508c2a986cd1
Arthur <0xa1337@proton.me>
2024-08-06 02:25:54 +0200

    feat: add cta to header

--- app/src/shared/components/header.tsx
+++ app/src/shared/components/header.tsx
@@ -1,5 +1,7 @@
+import { CirclePlusIcon, CompassIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 export function Header() {
   return (
@@ -13,6 +15,20 @@ export function Header() {
           className="h-8 w-auto"
         />
       </Link>
+      <div>
+        <Button size={"sm"} variant={"outline"} className="mr-2" asChild>
+          <Link href="/explore">
+            <CompassIcon className="w-4 h-4 mr-2" />
+            Explore
+          </Link>
+        </Button>
+        <Button size={"sm"} asChild>
+          <Link href="/new">
+            <CirclePlusIcon className="w-4 h-4 mr-2" />
+            Create
+          </Link>
+        </Button>
+      </div>
     </header>
   );
 }
