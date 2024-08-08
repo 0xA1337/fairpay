755af7cf57b9b5944ce8701d376928f7ad1893e4
Arthur <0xa1337@proton.me>
2024-08-09 01:45:19 +0200

    fix(app): wagmi cli not detecting env vars

--- app/src/core/config/wagmi-cli.ts
+++ app/src/core/config/wagmi-cli.ts
@@ -1,6 +1,10 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import { defineConfig } from "@wagmi/cli";
 import { actions, react } from "@wagmi/cli/plugins";
 import { Address, zeroAddress } from "viem";
+
 import { fairpayAbi } from "../abis/Fairpay";
 
 export default defineConfig({
@@ -9,7 +13,7 @@ export default defineConfig({
     {
       name: "Fairpay",
       abi: fairpayAbi,
-      address: (process.env.FAIRPAY_ADDRESS || zeroAddress) as Address,
+      address: (process.env.FAIRPAY_ADDRESS ?? zeroAddress) as Address,
     },
   ],
   plugins: [react(), actions()],
