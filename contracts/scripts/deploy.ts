c57ff83d2037f930bf353c7de7ea0bd21491b6c9
Arthur <0xa1337@proton.me>
2024-08-09 02:36:51 +0200

    ci(contracts): deploy mockUsdc token when on testnet

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,15 +1,19 @@
 import hre from "hardhat";
 
 async function main() {
-  const sepoliaUsdc = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
-  const mainnetUsdc = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
-  const usdc = hre.network.name === "base-sepolia" ? sepoliaUsdc : mainnetUsdc;
+  const network = hre.network.name;
 
-  const fairpay = await hre.viem.deployContract("Fairpay", [usdc]);
+  if (network === "base-sepolia") {
+    const mUsdc = await hre.viem.deployContract("MockUSDC", []);
+    console.log(`Deployed MockUSDC at ${mUsdc.address} on ${network}`);
+    const fairpay = await hre.viem.deployContract("Fairpay", [mUsdc.address]);
+    console.log(`Deployed Fairpay at ${fairpay.address} on ${network}`);
+    return;
+  }
 
-  console.log(
-    `Deploy Fairpay to ${hre.network.name} with address: ${fairpay.address}, using USDC: ${usdc}`
-  );
+  const usdcAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+  const fairpay = await hre.viem.deployContract("Fairpay", [usdcAddress]);
+  console.log(`Deployed Fairpay at ${fairpay.address} on ${network}`);
 }
 
 main().catch((error) => {
